21b2c03306488e5b9d32c845cce17b5fce602862
jhkane <kanej97@yahoo.com>
2015-10-16 21:32:29 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+    function myFunction() {
+	var numbertext = document.getElementById("number").innerHTML;
+	var number = parseInt(numbertext);
+	var newNumber = number + 1;
+	document.getElementById("number").innerHTML = newNumber;
+	
+	// make statements regarding value
+	if (newNumber==10)
+		{document.getElementById("result").innerHTML = "Your number is Ten"}
+	else if (newNumber<10)
+	{document.getElementById("result").innerHTML = "Your number is less than Ten";}
+
+	else if (newNumber>10)
+		{document.getElementById("result").innerHTML = "Your number:" +  newNumber + ", is greater than Ten";}
+	};
